ac0ad1e4f665a0e95b5e8923f890a3684063d186
ttroy50 <ttroy50@gmail.com>
2015-11-28 13:07:32 +0000

    add a configure_file example

---
+++ 03-code-generation/configure-files/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "ver.h"
+#include "path.h"
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello Version " << ver << "!" << std::endl;
+    std::cout << "Path is " << path << std::endl;
+   return 0;
+}
