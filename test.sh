3ce8377ced7f3466df9ccbbddafc0f4659c6d3eb
ttroy50 <ttroy50@gmail.com>
2015-12-01 23:05:48 +0000

    add unit-testing to my tests. All support for pre and post tests

--- test.sh
+++ test.sh
@@ -12,6 +12,7 @@ dirs=( ./01-basic/A-hello-cmake \
 ./03-code-generation/protobuf \
 ./03-code-generation/configure-files \
 ./04-static-analysis/cppcheck \
+./05-unit-testing/boost \
 )
 
 ROOT_DIR=`pwd`
@@ -22,14 +23,24 @@ do
     echo ""
     echo "Running test for $dir"
     cd $ROOT_DIR
-    if [ -d "$dir/build" ]; then
+    if [ -d "$ROOT_DIR/$dir/build" ]; then
         echo "deleting $dir/build"
         rm -r $dir/build
+    fi
+
+    if [ -f "$dir/pre_test.sh" ]; then
+        echo "running pre test"
+        $dir/pre_test.sh
     fi
 
     cd $dir && mkdir -p build && cd build && cmake .. && make
     if [ $? -ne 0 ]; then
         echo "Error running example $dir"
         exit 1
+    fi
+
+    if [ -f "$ROOT_DIR/$dir/post_test.sh" ]; then
+        echo "running post test"
+        $ROOT_DIR/$dir/post_test.sh
     fi
 done
