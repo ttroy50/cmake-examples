7f0c17e291f8dd0c04d5c11dd04ae8678d3d98c9
Jacob Hummer <jcbhmr@outlook.com>
2023-09-28 01:57:11 +0000

    fix test script to be just hello-world for now

--- tools/test-examples.sh
+++ tools/test-examples.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 set -e
 
-pushd c-app-using-quickjs-via-cpm-cmake
-cmake -Bbuild
+pushd hello-world
+cmake -B build
 cmake --build build
-popd
-
-pushd c-library-using-curl-via-fetchcontent
-cmake -Bbuild
-cmake --build build
-popd
-
-pushd cpp-app-using-ftxui-with-gtest-via-conan
-conan install . -s build_type=Debug --build=missing
-cmake --preset conan-debug
-cmake --build --preset conan-debug
 popd
