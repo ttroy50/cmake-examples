2df5b2089ba377ee8e97f4239e187c919f5c51f9
Jacob Hummer <jcbhmr@outlook.com>
2023-10-05 16:23:23 -0500

    try another method

--- public/replit-onBoot.sh
+++ public/replit-onBoot.sh
@@ -2,9 +2,11 @@
 set -e
 shopt -s nullglob
 shopt -s extglob
+shopt -s dotglob
 [[ $REPL_OWNER != jcbhmr || $REPL_SLUG != "cmakebyexampledev-$1" ]] && exit
-rm -rf $(ls | grep -v replit)
+rm -rf !(.replit|replit.nix)
 git clone https://github.com/jcbhmr/cmakebyexample.dev .git --bare
 git config --local --bool core.bare false
 git restore -s@ -SW -- "$1"
-mv -fv "$1"/* "$1"/.* ./
+mv -f "$1"/* ./
+rm -rf .git "$1"
