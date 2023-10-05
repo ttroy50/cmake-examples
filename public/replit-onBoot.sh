35cd7732a8ff757573c1258bad1ec27912b6d4b4
Jacob Hummer <jcbhmr@outlook.com>
2023-10-05 16:17:47 -0500

    try new onboot

--- public/replit-onBoot.sh
+++ public/replit-onBoot.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 shopt -s nullglob
-temp_dir=$(mktemp -d)
-git clone https://github.com/jcbhmr/cmakebyexample.dev "$temp_dir"
-rm -rf * .*
-mv -f "$temp_dir/$1"/* "$temp_dir/$1"/.* ./
+shopt -s extglob
+[[ $REPL_OWNER != jcbhmr || $REPL_SLUG != "cmakebyexampledev-$1" ]] && exit
+rm -rf $(ls | grep -v replit)
+git clone https://github.com/jcbhmr/cmakebyexample.dev .git --bare
+git config --local --bool core.bare false
+git restore -s@ -SW -- "$1"
+mv -fv "$1"/* "$1"/.* ./
